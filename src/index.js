bc6cb2799c11e212fd44a60913f878bea5f62196
Brendan Goodenough <brendan@goodenough.nz>
2017-05-24 08:10:30 +1200

    Connect Discord to AWS Lex

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
+import AWS from 'aws-sdk'
+import config from 'config'
 import Discord from 'discord.js'
+
 const client = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`)
-})
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+async function handleMessage (message) {
+  if (message.author.id === client.user.id) {
+    return
   }
-})
 
-client.login(process.env.DISCORD_TOKEN)
+  const channel = message.channel
+  const lexRuntime = new AWS.LexRuntime()
+  const params = {
+    botAlias: config.get('lex.botAlias'),
+    botName: config.get('lex.botName'),
+    inputText: message.content,
+    userId: config.get('lex.userId')
+  }
+
+  try {
+    const res = await lexRuntime.postText(params).promise()
+    message.reply(res.message)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+client.on('ready', () => console.log(`Logged in as ${client.user.username}!`))
+client.on('message', handleMessage)
+client.login(config.get('discord.token'))
