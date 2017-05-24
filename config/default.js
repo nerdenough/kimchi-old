ce2090c7a1fbde740df2112c8106096eaab1ac1c
Brendan Goodenough <brendan@goodenough.nz>
2017-05-24 21:57:33 +1200

    Setup testing

--- config/default.js
+++ config/default.js
@@ -1,11 +1,11 @@
 const config = {
   discord: {
-    token: process.env.DISCORD_TOKEN
+    token: process.env.DISCORD_TOKEN || ''
   },
   lex: {
-    botAlias: process.env.AWS_BOT_ALIAS,
-    botName: process.env.AWS_BOT_NAME,
-    userId: process.env.AWS_USER_ID
+    botAlias: process.env.AWS_BOT_ALIAS || '',
+    botName: process.env.AWS_BOT_NAME || '',
+    userId: process.env.AWS_USER_ID || ''
   }
 }
 
---
+++ tests/bot.test.js
@@ -0,0 +1,27 @@
+/* eslint-env mocha */
+import Discord from 'discord.js'
+import sinon from 'sinon'
+import { expect } from 'chai'
+import { initialise } from '../src/bot'
+
+describe('Bot', () => {
+  describe('#initialise', () => {
+    let clientOnStub
+    let clientLoginStub
+
+    before(() => {
+      clientOnStub = sinon.stub(Discord.Client.prototype, 'on').returns(true)
+      clientLoginStub = sinon.stub(Discord.Client.prototype, 'login').returns(true)
+      initialise()
+    })
+
+    after(() => {
+      clientOnStub.restore()
+      clientLoginStub.restore()
+    })
+
+    it('attempts to sign into discord', () => expect(clientLoginStub).to.have.been.called)
+    it('sets the ready callback', () => expect(clientOnStub).to.have.been.calledWith('ready'))
+    it('sets the message callback', () => expect(clientOnStub).to.have.been.calledWith('message'))
+  })
+})
